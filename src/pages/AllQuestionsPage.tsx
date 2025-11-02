2b40c44f4b67e88d9c3440af889a13aaa3968543
dorianpop1512@gmail.com <uratenie50@gmail.com>
2025-11-03 00:06:40 +0200

    Enhanced UI for all questions page

--- src/pages/AllQuestionsPage.tsx
+++ src/pages/AllQuestionsPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import generateAnswerKeyPdf from "../utils/GenerateAnswerKeyPDF"
 import generateQuizOnlyPdf from "../utils/GenerateQuizPDF"
 import { useInformationStore } from "../stores/informationStore"
+import "../css_files/SlideMCQGenerator.css"
 
 const AllQuestionsPage = () => {
     const cachedQuestions = useQuestionsStore((state) => state.cachedQuestions)
@@ -17,12 +18,12 @@ const AllQuestionsPage = () => {
     }
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", width: "50rem" }}>
-            <h1>Questions generated so far</h1>
+        <div className="generated-question-container" id="questions-container">
+            <h1 style={{ marginTop: "0.5rem" }}>Generated Questions</h1>
             <div style={{width:"100%"}}>
                 {cachedQuestions.map((question, index) => {
                     return (
-                        <button style={{width:"100%", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}} onClick={() => {setCurrentQuestion(index); nav("/generatedQuestion")}} key={index}> {question.question} </button>
+                        <button className="question-button" style={{width:"100%", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}} onClick={() => {setCurrentQuestion(index); nav("/generatedQuestion")}} key={index}> <div className="answer-choice">{index + 1}</div>  {question.question} </button>
                     )
                 })}
             </div>
@@ -30,19 +31,21 @@ const AllQuestionsPage = () => {
                 {questions.map((question, index) => {
                     if(index > informationStore.getIndex()) return;
                     return (
-                        <button style={{width:"100%", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}} onClick={() => {setCurrentQuestion(index); nav("/generatedQuestion")}} key={index}> {question.question} </button>
+                        <button className="question-button" style={{width:"100%", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}} onClick={() => {setCurrentQuestion(index); nav("/generatedQuestion")}} key={index}> <div className="answer-choice">{index + cachedQuestions.length + 1}</div> {question.question} </button>
                     )
                 })}
             </div>
-            <button style={{marginTop: "3rem"}} onClick={() => {
-                informationStore.setIndex(informationStore.getIndex() + 1);
-                nav("/question")
-                }}>
-                Generate New Question
-            </button>
-            <button onClick={generateQuizPDF}>
-                Export to PDF
-            </button>
+            <div className="control-buttons-container">
+                <button className="generated-question-button whole-line-buttons" onClick={() => {
+                    informationStore.setIndex(informationStore.getIndex() + 1);
+                    nav("/question")
+                    }}>
+                    Generate New Question
+                </button>
+                <button className="generated-question-button whole-line-buttons" onClick={generateQuizPDF}>
+                    Export to PDF
+                </button>
+            </div>
         </div>
     )
 }
